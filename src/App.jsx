4fc9c923afb0158d75ddb34d5cb326e47dafce21
Austin Tsai <182472802+Austin-Tsai@users.noreply.github.com>
2025-11-23 12:02:08 -0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -114,8 +114,8 @@ function App() {
           </div>
           {Object.entries(database).map(([time, data], index) => {
             if (searchTerm != '' && !data.metadata.title.toLowerCase().includes(searchTerm.toLowerCase()) 
-              && !data.url.toLowerCase().includes(searchTerm.toLowerCase())
-              && !data.metadata.author.toLowerCase().includes(searchTerm.toLowerCase())) return;
+              && !data.url.toLowerCase().includes(searchTerm.toLowerCase())) return;
+              // && !data.metadata.author.toLowerCase().includes(searchTerm.toLowerCase())) 
               // && !data.ml_keywords.toLowerCase().includes(searchTerm.toLowerCase())
               // && !data.metadata_json.html_keywords.toLowerCase().includes(searchTerm.toLowerCase())) return;
 
